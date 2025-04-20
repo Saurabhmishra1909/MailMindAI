1889d0d9a2276921696642d3a42b0cb5f2fa8a33
Saurabh Mishra <saurabhmishra3241@gmail.com>
2025-04-20 17:19:48 +0530

    Initial Commit

---
+++ email-writer-sb/src/main/java/com/email/writer/EmailWiterApp/EmailRequest.java
@@ -0,0 +1,20 @@
+package com.email.writer.EmailWiterApp;
+
+public class EmailRequest {
+    private String emailContent;
+    private String tone;
+    
+    public String getEmailContent() {
+        return emailContent;
+    }
+    public void setEmailContent(String emailContent) {
+        this.emailContent = emailContent;
+    }
+    public String getTone() {
+        return tone;
+    }
+    public void setTone(String tone) {
+        this.tone = tone;
+    }
+
+}
